923d6e8ade052526b41ee2f8d2aab43fb216588e
Fan Yang <me@minidump.info>
2018-09-14 08:06:23 +0800

    (*)package WIP

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"archive/zip"
+	"bytes"
 	"fmt"
 	"log"
 	"net/http"
@@ -131,6 +133,50 @@ func getFile(c *gin.Context) {
 		c.Data(http.StatusOK, "text/plain", data)
 		return
 	}
+	if file == "package.zip" {
+		var data []byte
+		switch platform {
+		case "windows":
+			data = packWin(gateway)
+		}
+
+		c.Data(http.StatusOK, "application/application/x-zip-compressed", data)
+		return
+	}
+}
+
+func packWin(gateway string) []byte {
+	// Create a buffer to write our archive to.
+	buf := new(bytes.Buffer)
+
+	// Create a new zip archive.
+	w := zip.NewWriter(buf)
+
+	// Add some files to the archive.
+	var files = []struct {
+		Name, Body string
+	}{
+		{"readme.txt", "This archive contains some text files."},
+		{"gopher.txt", "Gopher names:\nGeorge\nGeoffrey\nGonzo"},
+		{"todo.txt", "Get animal handling licence.\nWrite more examples."},
+	}
+	for _, file := range files {
+		f, err := w.Create(file.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = f.Write([]byte(file.Body))
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	// Make sure to check the error on Close.
+	err := w.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	return buf.Bytes()
 }
 
 func main() {
